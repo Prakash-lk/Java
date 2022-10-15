75a92d95db655b45223132ed772fef46024bbbe1
Prakash L <111338319+Prakash-lk@users.noreply.github.com>
2022-10-15 14:21:41 +0530

    Create KthLargestElement.java

---
+++ KthLargestElement.java
@@ -0,0 +1,37 @@
+# Java
+For Practice
+
+/*
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+You must solve it in O(n) time complexity.
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+*/
+
+#java
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    int tmp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=tmp;
+                }
+            }
+        }
+        int res=nums[nums.length-k];
+        return res;
+    }
+}
